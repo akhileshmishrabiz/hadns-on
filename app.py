f767447475ba91e67df466e245bf456c6c8daf12
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-07-27 16:32:07 +0530

    code changes (#7)
    * new branch push
    
    * new branch push
    
    * new branch push
    
    * new branch push
    
    * new branch push
    
    * workflow on pr
    
    * docker build workflow
    
    * fixed sha error
    
    * code changes

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return 'Hello, World today is sat - this is second run at 7 pm and this is automated third code change'
+    return '''
+
+            <h1>Hello, World </h1>
+            <P>today this is second run at 7 pm and this is automated third code change</P>
+            '''
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
